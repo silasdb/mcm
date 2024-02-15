15e95fb2c2007a8db39ba25a4d1b2402de36efb2
silas <silas_jfex@silas.net.br>
2024-02-15 20:43:19 +0000

    modules-experimental/user: Support the adduser command as well as more classic useradd
    FossilOrigin-Name: a433e2fb6d2b9ae97de4b9e171429b81774a415ad6a17ed6d555d464448d91d0

--- modules-experimental/user/user.execute.sh
+++ modules-experimental/user/user.execute.sh
@@ -1,3 +1,21 @@
-#!/bin/sh
 # TODO: handle case where $HOME is not necessary
-useradd -m "$username"
+# TODO: what about passing a password?
+
+which useradd && {
+	if [ -n "${home+x}" ]; then
+		useradd -m -d "$home" "$username"
+	else
+		useradd -m "$username"
+	fi
+	exit
+}
+
+which adduser && {
+	# -D: don't assign a password
+	if [ -n "${home+x}" ]; then
+		adduser -D -h "$home" "$username"
+	else
+		adduser -D "$username"
+	fi
+	exit
+}
