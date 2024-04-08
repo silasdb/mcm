42dee7d864ed3e78701bec58e2c81dca5e386b94
silas <silas_jfex@silas.net.br>
2024-04-08 21:44:45 +0000

    modules-experimental/download/download.execute.sh: support curl and use a function as a wrapper to which to check existence of those tools.
    FossilOrigin-Name: 2e36cbc768c579b9b2b3972a6b8e3eacdd4e5c6cda98379a3eb5cd352987bf09

--- modules-experimental/download/download.execute.sh
+++ modules-experimental/download/download.execute.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
-if which wget >/dev/null; then
+
+tool_exists () {
+	which "$1" >/dev/null 2>/dev/null
+}
+
+if tool_exists curl; then
+	curl -o "$path" "$url"
+elif tool_exists wget; then
 	wget -O "$path" "$url"
-elif which ftp >/dev/null; then
+elif tool_exists ftp; then
+	# TODO: test this code path on GNU/Linux. Do ftp client on GNU/Linux
+	# work like we expect?
 	ftp -o "$path" "$url"
 else
 	echo 'No download tool found' >&2
