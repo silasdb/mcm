a7285f9dbe0a2bd554f11d07b318e507b730b24a
silas <silas_jfex@silas.net.br>
2024-04-18 01:38:13 +0000

    modules-experimental/pkg_openbsd/pkg_openbsd.test.sh: fix test phase for pkg_openbsd module
    FossilOrigin-Name: a9508ed6278322000693d2420fc50fe972e1f6235cf64f0dda207ee8a43d43a2

--- modules-experimental/pkg_openbsd/pkg_openbsd.test.sh
+++ modules-experimental/pkg_openbsd/pkg_openbsd.test.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-# TODO: not the best way to check for the existence of a package, since it is
-# ambiguous. E.g.: both foo and foo-bar are matched.
-test -d "/var/db/pkg/$name"*
+pkg_info -Iq inst:"$name"
