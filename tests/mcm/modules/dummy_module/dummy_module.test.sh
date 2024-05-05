c0ce198356911feda46c73f4456987c20f37d8fd
silas <silas_jfex@silas.net.br>
2024-05-05 22:51:48 +0000

    tests/mcm/modules/dummy_module/dummy_module.test.sh: add comment to dummy_module that can trigger an error in mcm.
    FossilOrigin-Name: b41b520aaaf842bf8629a0c44934acc7a0899edb2e8203b2db295b0361023b4d

--- tests/mcm/modules/dummy_module/dummy_module.test.sh
+++ tests/mcm/modules/dummy_module/dummy_module.test.sh
@@ -2,4 +2,11 @@
 
 test -n "${test_out+x}" && echo "$test_out"
 test -n "${test_err+x}" && echo "$test_err" >&2
+
+# A comment with string "\n" can break mcm, as mcm will expand modules content
+# to "<a new line>", so a newline that starts with character >"< is appended,
+# which is a syntax error. We add this comment here to reproduce this error and
+# fix it in mcm. Note that it comes before exit, because nothing after exit is
+# executed, right or wrong.
+
 exit "$test_ret"
