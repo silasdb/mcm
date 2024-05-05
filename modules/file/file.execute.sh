893b4185e975e87afc7755ee7d5acc09a081dd39
silas <silas_jfex@silas.net.br>
2024-05-05 22:32:58 +0000

    modules/file: add TODO comment for an edge case to take a look later
    FossilOrigin-Name: 216228de66ffc780c9f56581cb7ae9289bf0131d0b126a872b96171b3fa6fd8f

--- modules/file/file.execute.sh
+++ modules/file/file.execute.sh
@@ -4,5 +4,7 @@
 
 # TODO: What if $path is a symlink?
 if [ -n "${content+x}" -a \( ! -f "$path" -o "$force" = 'yes' \) ]; then
+	# TODO: test echo vs printf for cases where both origin and target have
+	# the same content, except if the last character in the file is "\n".
 	echo "$content" > "$path"
 fi
