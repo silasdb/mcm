ddad442ef139f896dae58aa1156b2e7c95c9ed27
silas <silas_jfex@silas.net.br>
2024-06-20 01:18:18 +0000

    modules/file: handling EOL in the end of the line is tricky in a shell script... so prefer to always append a \n to the content of a file, unless eofchar is set to other character than \n
    FossilOrigin-Name: 58c3ec937e713be366e4cc889f8b7085d42cbff22e67e152a611c89c3fd49a31

--- modules/file/file.execute.sh
+++ modules/file/file.execute.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 : ${force:='yes'}
+: ${eofchar='\n'}
 
 # TODO: What if $path is a symlink?
 if [ -n "${content+x}" -a \( ! -f "$path" -o "$force" = 'yes' \) ]; then
 	# TODO: test echo vs printf for cases where both origin and target have
 	# the same content, except if the last character in the file is "\n".
-	echo "$content" > "$path"
+	printf "%s$eofchar" "$content" > "$path"
 fi
