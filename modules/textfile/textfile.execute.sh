0c9ec8762d81df75b37c209bd333c88211347709
silas <silas_jfex@silas.net.br>
2024-07-27 23:58:53 +0000

    modules/textfile: adapt test phase file to use printf like execute phase file does. Remove outdated comments.
    FossilOrigin-Name: f7c5627661da96e2d5408a035a4095c83a82da12d10e91a7e0b283431f6851b6

--- modules/textfile/textfile.execute.sh
+++ modules/textfile/textfile.execute.sh
@@ -5,7 +5,5 @@
 
 # TODO: What if $path is a symlink?
 if [ -n "${content+x}" -a \( ! -f "$path" -o "$force" = 'yes' \) ]; then
-	# TODO: test echo vs printf for cases where both origin and target have
-	# the same content, except if the last character in the file is "\n".
 	printf "%s$eofchar" "$content" > "$path"
 fi
--- modules/textfile/textfile.test.sh
+++ modules/textfile/textfile.test.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 : ${force:='yes'}
+: ${eofchar:='\n'}
 
 # content is optional (one may want to set file permissions only).  If content
 # is set, we check if the file does not exist OR if $force flag is set.
 if [ -n "${content+x}" -a \( ! -f "$path" -o "$force" = 'yes' \) ]; then
-	# TODO: test echo vs printf for cases where both origin and target have
-	# the same content, except if the last character in the file is "\n".
-	echo "$content" | cmp -s - "$path"
+	printf "%s$eofchar" "$content" | cmp -s - "$path"
 fi
 
