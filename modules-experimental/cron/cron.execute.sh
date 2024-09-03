6b3f861cfefc4e6976d91a1536a39d3a9bd9f2bf
silas <silas_jfex@silas.net.br>
2024-09-03 18:35:14 +0000

    modules-experimental/cron/cron.execute.sh: only one case is needed to edit the crontab. Try removing the current entry anyway since, if it doesn't exist, no harm will be caused. Simplify even more! ♥
    FossilOrigin-Name: af3d09b05bf08837ef22441ddc9eac5894fb972087b1ec40a71a25e948241f5c

--- modules-experimental/cron/cron.execute.sh
+++ modules-experimental/cron/cron.execute.sh
@@ -2,27 +2,15 @@
 
 # TODO: don't use $PWD here. Create a temporary directory instead.
 # TODO: check if time_spec is set?
+# TODO: what if $name has a slash?
 
 uflag=
 if [ -n "${user+x}" ]; then
 	uflag="-u ${user}"
 fi
 
-# If such entry doesn't exist in crontab, just append it.
-if ! crontab $uflag -l | grep -q -x -F "# MCM: $name"; then
-	# If the register is not added, just appends it at the end of the
-	# crontab.
-	{
-		crontab -l
-		echo "# MCM: $name"
-		echo "$time_spec $job"
-	} | crontab -
-	exit
-fi
-
-# If entry exists in crontab, replace it.
 {
-	# TODO: what if $name has a slash?
+	# Remove current entry, if it exists.
 	crontab -l | sed -n "
 		:begin
 			/^# MCM: $name\$/ b found
